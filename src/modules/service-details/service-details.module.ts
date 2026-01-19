a5cae9716c5f02ced7df66311876caaa85fa9bcf
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-20 00:20:14 +0600

    module setuped

--- src/modules/service-details/service-details.module.ts
+++ src/modules/service-details/service-details.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AcServiceDetails } from './entities/ac-service-details.entity';
+import { CctvServiceDetails } from './entities/cctv-service-details.entity';
+import { ServiceDetailsService } from './service-details.service';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      AcServiceDetails,
+      CctvServiceDetails
+    ])
+  ],
+  providers: [ServiceDetailsService],
+  exports: [ServiceDetailsService]
+})
 export class ServiceDetailsModule {}

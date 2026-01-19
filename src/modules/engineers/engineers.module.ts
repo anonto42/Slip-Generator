01f05a7d9ad2590edd8e0007065dc59bf94570df
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-20 00:21:17 +0600

    engineer type module setuped

--- src/modules/engineers/engineers.module.ts
+++ src/modules/engineers/engineers.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EngineersController } from './engineers.controller';
 import { EngineersService } from './engineers.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Engineer } from './entities/engineer.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Engineer])],
   controllers: [EngineersController],
   providers: [EngineersService],
 })
